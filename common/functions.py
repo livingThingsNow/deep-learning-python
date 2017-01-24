cd7e0c778aa9bbedeba3c6128ca82a81d1dd788d
livingThingsNow <livingThingNOw@gmail.com>
2017-01-24 22:28:06 +0900

    This is NeuralNetwork script.

--- common/functions.py
+++ common/functions.py
@@ -30,6 +30,21 @@ def softmax(x):
     x = x - np.max(x) # オーバーフロー対策
     return np.exp(x) / np.sum(np.exp(x))
 
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+
 
 def softmax_loss(X, t):
     y = softmax(X)
